943332718d04e0b289474e1c90e7b913ad2bac99
Tetiana Baranova <syniovych.tania@gmail.com>
2025-01-26 22:15:20 +0200

    initial and Description

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import './App.css'
+import Description from './components/Description/Description'
+
+function App() {
+
+    return (
+        <>
+        <Description />
+        </>
+    );
+}
+
+export default App
